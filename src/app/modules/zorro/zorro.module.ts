40fc574cf34475dc009da33b0d2d3315ff986760
nudopnu <forsuse@live.de>
2024-03-07 15:00:44 +0100

    Added zorro

---
+++ src/app/modules/zorro/zorro.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NzInputModule } from 'ng-zorro-antd/input';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    NzInputModule,
+  ]
+})
+export class ZorroModule { }
